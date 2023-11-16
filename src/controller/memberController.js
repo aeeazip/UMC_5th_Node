7f05b9c258580f9b767c7616a42326ea7f63fc07
aeeazip <codnjs0809@naver.com>
2023-11-16 22:31:03 +0900

    feat(#1) : 회원가입 API 작성

--- src/controller/memberController.js
+++ src/controller/memberController.js
@@ -1,6 +1,12 @@
 import { status } from "../../config/responseStatus.js";
 import { response } from "../../config/response.js";
 
+// 0. 테스트 API
 export const getUser = (req, res, next) => {
   res.send(response(status.SUCCESS, "This is test"));
+};
+
+export const userSignup = async (req, res, next) => {
+  console.log("회원가입을 요청하였습니다!");
+  res.send(response(status.SUCCESS, await joinUser(req.body)));
 };
